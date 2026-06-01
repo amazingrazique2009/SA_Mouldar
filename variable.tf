c0869dc34585ba6ffbafa8be2b5c05b53aed1fde
Razique <145266460+amazingrazique2009@users.noreply.github.com>
2026-06-01 11:31:42 +0530

    Add variables for Terraform configuration

---
+++ variable.tf
@@ -0,0 +1,25 @@
+variable "resource_group_name" {
+ description = name of resource group
+ type        = string
+ 
+}
+variable "location"  {
+ description = resource location/region
+ type        = string
+ 
+}
+variable "storage_account_name" {
+ description = name of the storage account
+ type        = string
+ 
+}
+variable "access_tier" {
+ description = access tier
+ type        = string
+ 
+}
+variable "account_replication_type" {
+ description = replictio type
+ type        = string
+ 
+}
